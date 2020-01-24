b0fca19df68ca72dc9fca04c9ae827a59e103dfe
Delfio <delfio_eu@hotmail.com>
2020-01-23 23:31:24 -0400

    Header principal

--- src/components/MenuDefault/index.js
+++ src/components/MenuDefault/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import Logo from '../../assets/logoParecis.svg'
+
+import { Container, Button } from './styles';
 
 export default function MenuDefault() {
+
+  const [play, setPlay] = useState(false);
+
+  function playAudio(){
+    var x = document.getElementById("player");
+    if(!play){
+      x.play()
+      setPlay(true)
+    }else{
+      x.pause()
+      setPlay(false)
+    }
+
+  }
+
   return (
-    <div />
+    <Container>
+      <div className="nav-wrapper">
+        <audio id="player" src="http://live.hunter.fm/live"></audio>
+        <div className="container">
+          <a href="#" className="brand-logo">
+            <div className="col s12">
+              <img className="responsive-img" style={{width: '100%', height: '100%', maxWidth: '80px', maxHeight: '150px', marginTop: 5}} src={Logo} alt=""/>
+            </div>
+          </a>
+        </div>
+        <ul style={{marginRight: '4.5em'}} id="nav-mobile" className="right hide-on-med-and-down">
+          <li style={{marginRight: '1.5rem'}}>
+            <Button className="btn waves-effect waves-light" onClick={playAudio}>
+              AO VIVO
+              <i className="material-icons right">send</i>
+            </Button>
+          </li>
+          <li style={{marginRight: '0.5em'}}><a href="sass.html">PEÇA SUA MÚSICA</a></li>
+          <li style={{marginRight: '0.5em'}}><a href="badges.html">AS MAIS PEDIDAS</a></li>
+          <li style={{marginRight: '0.5em'}}><a href="collapsible.html">PROMOÇÕES</a></li>
+          <li style={{marginRight: '0.5em'}}><a href="collapsible.html">PROGRAMAÇÃO</a></li>
+          <li><a href="collapsible.html">CONTATOS</a></li>
+        </ul>
+      </div>
+    </Container>
   );
 }
---
+++ src/components/MenuDefault/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap');
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 600;
+  background-image: linear-gradient(to bottom, #c0242b, #e3353d);
+`;
+
+export const Button = styled.button`
+  background-color: #9e1e24;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  border-radius: 7px;
+  box-shadow: 0 5px #891a1f;
+  i{
+    display: flex;
+    position: top;
+    margin-top: -13px
+  }
+  &:hover{
+    color: #e5e5e5;
+    background-color: #86181d; 
+  }
+`;
