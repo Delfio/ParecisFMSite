13cd07dec50ac314bcef529efb14c136d01cca59
Delfio <delfio_eu@hotmail.com>
2020-01-26 00:07:09 -0400

    Radiu Mobile - designer

--- src/components/RadioMobile/index.js
+++ src/components/RadioMobile/index.js
@@ -4,9 +4,14 @@ import React from 'react';
 
 export default function RadioMobile() {
   return (
-    <div className="row hide-on-med-and-up">
-      <div className="col s12 center">
-        <audio controls id="player" src="http://live.hunter.fm/live"></audio>
+    <div className="row hide-on-large-only">
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} className="col s12 center">
+        <audio 
+          style={{
+            boxShadow: '0 2px 5px rgba(0,0,0,0.5)',
+            borderRadius: 17,
+            marginTop: 15
+          }} controls id="player" src="http://live.hunter.fm/live"></audio>
       </div>
     </div>
   );
