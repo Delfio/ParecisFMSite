dc98079926b2690bcdc01e5b921d0495e6f8933c
Delfio <delfio_eu@hotmail.com>
2020-02-05 22:32:58 -0400

    connetion websocket - client

---
+++ src/services/socket.js
@@ -0,0 +1,20 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio(process.env.REACT_APP_API_URL, {
+  autoConnect: false,
+});
+
+function connect() {
+  socket.connect()
+}
+
+function disconnect() {
+  if(socket.connected){
+    socket.disconnect()
+  }
+}
+
+export {
+  connect,
+  disconnect
+}
