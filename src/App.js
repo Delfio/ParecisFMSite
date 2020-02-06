8915d23cfe845aa3e88bfb082b43c8a6baa26f33
Delfio <delfio_eu@hotmail.com>
2020-02-06 01:54:58 -0400

    persisted infos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import Routes from './routes';
-import store from './store'
+import {store, persistor} from './store'
 import history from './services/history';
 
 import GlobalStyle from './styles/global'
@@ -14,11 +14,13 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <ToastContainer autoClose={2500} />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <ToastContainer autoClose={2500} />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 import createStore from './createStorage';
+
+import persistedReducers from './persistReducers'
 
 import  rootReducer from './modules/rootReducer';
 import  rootSaga from './modules/rootSaga';
@@ -12,8 +15,9 @@ const sagaMiddlewares = createSagaMiddleware( {sagaMonitor} )
 
 const middlewares = [sagaMiddlewares];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persistedReducers(rootReducer), middlewares);
+const persistor = persistStore(store)
 
 sagaMiddlewares.run(rootSaga);
 
-export default store;
+export {store, persistor};
