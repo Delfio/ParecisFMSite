7c590c8e0dc3dc2c8e6e36845ddde75526f6e4da
Delfio <delfio_eu@hotmail.com>
2020-02-26 21:38:50 -0400

    fixx 'As mais pedidas - top3'

--- src/pages/Main/AsMaisPedidas.js
+++ src/pages/Main/AsMaisPedidas.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 // import api from '../../services/api'
 
 // import {toast} from 'react-toastify';
 
-import { SectionIMG, Ul, OuvirButton } from './styles';
-import { Link } from 'react-router-dom';
+import { SectionIMG, Ul, OuvirButton } from "./styles";
+import { Link } from "react-router-dom";
 
-export default function AsMaisPedidas({top3, id: RadioID}) {
+export default function AsMaisPedidas({ top3, id: RadioID }) {
   const infos = top3;
 
   // const [date, setDate] = useState(new Date());
@@ -27,27 +27,28 @@ export default function AsMaisPedidas({top3, id: RadioID}) {
   return (
     <div className="row">
       <div id="maispedidas" className="container">
-        <h4 style={{fontWeight: 900}} className="center-align grey-text">AS MAIS PEDIDAS - TOP 3</h4>
+        <h4 style={{ fontWeight: 900 }} className="center-align grey-text">
+          TOP 3
+        </h4>
         <Ul className="col s12">
           {infos.map(el => (
             <li key={el.id} className="col l4 s12 center-align">
-            <SectionIMG
-              bg={el.image? el.image.url : null}
-              >
-               <div className="row">
-                <h5 className="white-text">{el.artista}</h5>
-                <p>{el.musica}</p>
-               </div>
+              <SectionIMG bg={el.image ? el.image.url : null}>
+                <div className="row">
+                  <h5 className="white-text">{el.artista}</h5>
+                  <p>{el.musica}</p>
+                </div>
               </SectionIMG>
               <Link to={`/top3/${el.id}/escutar/${RadioID}`}>
                 <OuvirButton
                   className="btn waves-effect waves-light red darken-3"
                   type="submit"
                   name="action"
-                    >Ouvir
+                >
+                  Ouvir
                 </OuvirButton>
               </Link>
-          </li>
+            </li>
           ))}
         </Ul>
       </div>
