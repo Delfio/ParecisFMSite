c47a9a7d5cb1aa44e81c88d275ce66fa5c8ac2e1
Delfio <delfio_eu@hotmail.com>
2020-03-12 22:06:18 -0400

    Fixx image size icon

--- src/pages/Dashboard/Radio/EnviarBanner.js
+++ src/pages/Dashboard/Radio/EnviarBanner.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
-import 'react-dropzone-uploader/dist/styles.css';
-import Dropzone from 'react-dropzone-uploader';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import "react-dropzone-uploader/dist/styles.css";
+import Dropzone from "react-dropzone-uploader";
+import { useSelector } from "react-redux";
 
-import { toast } from 'react-toastify';
-import api from '../../../services/api';
+import { toast } from "react-toastify";
+import api from "../../../services/api";
 
 // import { Container } from './styles';
 const secret = process.env.REACT_APP_KEY_SECRET_KEY_WITH_A_AUTHENTICATION;
 
 export default function EnviarBanner(props) {
-
   const idAdm = props.match.params.id;
   const profile = useSelector(state => state.user.profile);
 
   const getUploadParams = async ({ file, meta }) => {
     try {
       const body = new FormData();
-      body.append('fileField', file);
+      body.append("fileField", file);
 
       const data = new FormData();
 
-      data.append('file', file);
+      data.append("file", file);
 
-      if(idAdm && profile.config === secret){
+      if (idAdm && profile.config === secret) {
         await api.post(`banner/${idAdm}/1`, data);
-      } else{
+      } else {
         await api.post(`banner/${profile.radio_id}/1`, data);
       }
 
@@ -34,84 +33,80 @@ export default function EnviarBanner(props) {
       // const { imagens } = response.data;
       // setImages(imagens);
 
-      toast.success('imagem cadastrada com sucesso');
+      toast.success("imagem cadastrada com sucesso");
 
-      return { url: 'https://httpbin.org/post', body };
+      return { url: "https://httpbin.org/post", body };
     } catch (err) {
-      toast.error('Algo deu errado! ');
+      toast.error("Algo deu errado! ");
     }
   };
 
   const getUploadParamsMobile = async ({ file, meta }) => {
     try {
       const body = new FormData();
-      body.append('fileField', file);
+      body.append("fileField", file);
 
       const data = new FormData();
 
-      data.append('file', file);
+      data.append("file", file);
 
-      if(idAdm && profile.config === secret){
+      if (idAdm && profile.config === secret) {
         await api.post(`banner/${idAdm}/2`, data);
-
       } else {
         await api.post(`banner/${profile.radio_id}/2`, data);
-
       }
       // const response = await api.get(`principal/${id}`);
 
       // const { imagens } = response.data;
       // setImages(imagens);
 
-      toast.success('imagem cadastrada com sucesso');
+      toast.success("imagem cadastrada com sucesso");
 
-      return { url: 'https://httpbin.org/post', body };
+      return { url: "https://httpbin.org/post", body };
     } catch (err) {
-      toast.error('Algo deu errado! ');
+      toast.error("Algo deu errado! ");
     }
   };
 
   const getUploadParamsIcon = async ({ file, meta }) => {
     try {
       const body = new FormData();
-      body.append('fileField', file);
+      body.append("fileField", file);
 
       const data = new FormData();
 
-      data.append('file', file);
+      data.append("file", file);
 
       const iconCad = await api.post(`/icon`, data);
       // const response = await api.get(`principal/${id}`);
 
       // const { imagens } = response.data;
       // setImages(imagens);
-      relationIconInRadio(iconCad.data.id);
+      await relationIconInRadio(iconCad.data.id);
 
-      return { url: 'https://httpbin.org/post', body };
+      return { url: "https://httpbin.org/post", body };
     } catch (err) {
-      toast.error('Algo deu errado! ');
+      toast.error("Algo deu errado! ");
     }
   };
 
   async function relationIconInRadio(iconId) {
     try {
-
-      if(idAdm && profile.config === secret){
+      if (idAdm && profile.config === secret) {
         await api.put(`radio/${idAdm}`, {
           icon_id: iconId
         });
-
       } else {
         await api.put(`radio/${profile.radio_id}`, {
           icon_id: iconId
         });
       }
-      
-      toast.success('Icone Cadastrado com sucesso');
+
+      toast.success("Icone Cadastrado com sucesso");
 
       return true;
-    } catch(err){
-      toast.error('Algo deu errado! ');
+    } catch (err) {
+      toast.error("Algo deu errado! ");
 
       return false;
     }
@@ -137,7 +132,7 @@ export default function EnviarBanner(props) {
           />
         </div>
       </div>
-      <br/>
+      <br />
       <div className="row">
         <div className="col s12 m10 offset-m1 xl12 offset-xl1 left-align">
           <div className="col s12 center">
@@ -156,7 +151,7 @@ export default function EnviarBanner(props) {
           />
         </div>
       </div>
-      <br/>
+      <br />
       <div className="row">
         <div className="col s12 m10 offset-m1 xl12 offset-xl1 left-align">
           <div className="col s12 center">
@@ -170,7 +165,7 @@ export default function EnviarBanner(props) {
             inputContent="Selecione ou arraste o icone"
             submitButtonContent="Enviar"
             accept="image/*"
-            maxSizeBytes={2024 * 2024}
+            maxSizeBytes={4048 * 4048}
             maxFiles={1}
           />
         </div>
