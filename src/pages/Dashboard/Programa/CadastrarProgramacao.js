dc24a6ec8ea20a0e9c3ccf5974bfe69e38394ae9
Delfio <delfio_eu@hotmail.com>
2020-03-15 12:59:34 -0400

    cad programacao com horario quebrado

--- src/pages/Dashboard/Programa/CadastrarProgramacao.js
+++ src/pages/Dashboard/Programa/CadastrarProgramacao.js
@@ -1,24 +1,23 @@
-import React, {useEffect} from 'react';
-import { Form } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
-import * as Yup from 'yup'
-import Table from './Table'
-import api from '../../../services/api'
+import React, { useEffect } from "react";
+import { Form, Input } from "@rocketseat/unform";
+import { useSelector } from "react-redux";
+import * as Yup from "yup";
+import Table from "./Table";
+import api from "../../../services/api";
 // import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-import Selected from './Selected';
+import Selected from "./Selected";
 
 const schema = Yup.object().shape({
-  horarios: Yup.string().required('Selecione algum horario'),
-  dia: Yup.string().required('Selecione algum Dia'),
-  locutor: Yup.string().required('Selecione algum Locutor'),
-  programa: Yup.string().required('Selecione algum Programa'),
-
-})
+  hora: Yup.string().required("Selecione uma hora"),
+  minuto: Yup.string().required("Selecione algum minuto"),
+  dia: Yup.string().required("Selecione algum Dia"),
+  locutor: Yup.string().required("Selecione algum Locutor"),
+  programa: Yup.string().required("Selecione algum Programa")
+});
 
 export default function CadastrarProgramacao() {
-
   //Suave
 
   const profile = useSelector(state => state.user.profile);
@@ -31,124 +30,183 @@ export default function CadastrarProgramacao() {
   const conteudo = [];
 
   const programa = [];
-  
-  async function loadLocutores(){
-    const response = await api.get(`/locutores/${profile.radio_id}`)
-    const {data} = response;
 
-    data.map(el => conteudo.push({id: el.id, title: el.name}));
+  async function loadLocutores() {
+    const response = await api.get(`/locutores/${profile.radio_id}`);
+    const { data } = response;
+
+    data.map(el => conteudo.push({ id: el.id, title: el.name }));
   }
 
-  async function loadProgramas () {
-    const response = await api.get('/programa');
-    const {data} = response;
+  async function loadProgramas() {
+    const response = await api.get("/programa");
+    const { data } = response;
 
-    data.map(el => programa.push({id: el.id, title: el.nome}));
+    data.map(el => programa.push({ id: el.id, title: el.nome }));
   }
 
-  async function loadProgramacao(data, { resetForm }){
-    const { horarios } =data;
+  async function loadProgramacao(data, { resetForm }) {
+    try {
+      const { hora, minuto } = data;
 
-    // const horaFormatada = horarios.join(',');
+      // const horaFormatada = horarios.join(',');
+      var horarios = new Array();
+      var minutos = new Array();
 
-    const resplace = horarios.replace(/[,]+/g, ' ');
-    
-    await api.post('/programacaos', {
-      horario: resplace,
-      programa_id: data.programa,
-      dia_id: data.dia,
-      user_id: data.locutor,
-      radio_id: profile.radio_id
-    });
+      var horaFormatada = "";
 
-    resetForm();
-    toast.success('Programação cadastrada com Sucesso')
+      horarios.push(hora.split(","));
+      minutos.push(minuto.split(","));
+
+      var lengthHorario = horarios[0].length;
+      var lengthMinutos = minutos[0].length;
+
+      const horariosFormated = horarios[0];
+      const minutosFormated = minutos[0];
+
+      if (lengthHorario != lengthMinutos) {
+        return toast.error(
+          "Verifique os horarios e minutos! cada horario deve ter um minuto na mesma sequência"
+        );
+      }
+
+      for (let index = 0; index < horariosFormated.length; index++) {
+        if (horaFormatada.length === 0) {
+          horaFormatada = `${horariosFormated[index]}:${minutosFormated[index]}`;
+        } else {
+          horaFormatada += ` ${horariosFormated[index]}:${minutosFormated[index]}`;
+        }
+      }
+
+      console.log(horaFormatada);
+
+      await api.post("/programacaos", {
+        horario: horaFormatada,
+        programa_id: data.programa,
+        dia_id: data.dia,
+        user_id: data.locutor,
+        radio_id: profile.radio_id
+      });
+
+      loadProgramas();
+      resetForm();
+      toast.success("Programação cadastrada com Sucesso");
+    } catch (err) {
+      toast.error("Algo deu errado! Confira seus dados");
+    }
   }
 
   const dias = [
-    { id: 1, title: 'Domingo' },
-    { id: 2, title: 'Segunda' },
-    { id: 3, title: 'Terça' },
-    { id: 4, title: 'Quarta' },
-    { id: 5, title: 'Quinta' },
-    { id: 6, title: 'Sexta' },
-    { id: 7, title: 'Sábado' },
-  ]
+    { id: 1, title: "Domingo" },
+    { id: 2, title: "Segunda" },
+    { id: 3, title: "Terça" },
+    { id: 4, title: "Quarta" },
+    { id: 5, title: "Quinta" },
+    { id: 6, title: "Sexta" },
+    { id: 7, title: "Sábado" }
+  ];
 
   const horas = [
-    { id:'00:00', title: '00:00' },
-    { id: '01:00', title: '01:00' },
-    { id: '02:00', title: '02:00' },
-    { id: '03:00', title: '03:00' },
-    { id: '04:00', title: '04:00' },
-    { id: '05:00', title: '05:00' },
-    { id: '06:00', title: '06:00' },
-    { id: '07:00', title: '07:00' },
-    { id: '08:00', title: '08:00' },
-    { id: '09:00', title: '09:00' },
-    { id: '10:00', title: '10:00' },
-    { id: '11:00', title: '11:00' },
-    { id: '12:00', title: '12:00' },
-    { id: '13:00', title: '13:00' },
-    { id: '14:00', title: '14:00' },
-    { id: '15:00', title: '15:00' },
-    { id: '16:00', title: '16:00' },
-    { id: '17:00', title: '17:00' },
-    { id: '18:00', title: '18:00' },
-    { id: '19:00', title: '19:00' },
-    { id: '20:00', title: '20:00' },
-    { id: '21:00', title: '21:00' },
-    { id: '22:00', title: '22:00' },
-    { id: '23:00', title: '23:00' },
-  ]
+    { id: "00", title: "00" },
+    { id: "01", title: "01" },
+    { id: "02", title: "02" },
+    { id: "03", title: "03" },
+    { id: "04", title: "04" },
+    { id: "05", title: "05" },
+    { id: "06", title: "06" },
+    { id: "07", title: "07" },
+    { id: "08", title: "08" },
+    { id: "09", title: "09" },
+    { id: "10", title: "10" },
+    { id: "11", title: "11" },
+    { id: "12", title: "12" },
+    { id: "13", title: "13" },
+    { id: "14", title: "14" },
+    { id: "15", title: "15" },
+    { id: "16", title: "16" },
+    { id: "17", title: "17" },
+    { id: "18", title: "18" },
+    { id: "19", title: "19" },
+    { id: "20", title: "20" },
+    { id: "21", title: "21" },
+    { id: "22", title: "22" },
+    { id: "23", title: "23" }
+  ];
+
+  const minutos = [
+    { id: "00", title: "00" },
+    { id: "20", title: "20" },
+    { id: "25", title: "25" },
+    { id: "30", title: "30" },
+    { id: "35", title: "35" },
+    { id: "40", title: "40" },
+    { id: "45", title: "45" }
+  ];
 
   return (
     <div className="container">
       <div className="row">
-        <div style={{marginTop: 50}} className="col s12 m10 offset-m1 xl12 offset-xl1 left-align">
+        <div
+          style={{ marginTop: 50 }}
+          className="col s12 m10 offset-m1 xl12 offset-xl1 left-align"
+        >
           <h3>Progamações</h3>
 
           <div className="row">
-            <Form style={{color: 'red'}} schema={schema} onSubmit={loadProgramacao}>
+            <Form
+              style={{ color: "red" }}
+              schema={schema}
+              onSubmit={loadProgramacao}
+            >
               <div className="row">
-               <div className="col l6 s12">
-                <Selected 
-                    name="dia" 
-                    label="Selecione o Dia" 
-                    options={dias}
+                <div className="col l6 s12">
+                  <Selected name="dia" label="Selecione o Dia" options={dias} />
+                </div>
+                <div className="col l6 s12">
+                  <Selected
+                    name="locutor"
+                    label="Selecione o Locutor Principal"
+                    options={conteudo}
                   />
-               </div>
-               <div className="col l6 s12">
-                <Selected 
-                    name="horarios" 
-                    label="Selecione os Horários" 
+                </div>
+                <div className="col l6 s12">
+                  <Selected
+                    name="programa"
+                    label="Selecione o nome do Programa"
+                    options={programa}
+                  />
+                </div>
+                <div className="col l3 s6">
+                  <Selected
+                    name="hora"
+                    label="Selecione a hora"
                     options={horas}
                     multiple={true}
                   />
-               </div>
-               <div className="col l6 s12">
-                <Selected 
-                    name="locutor" 
-                    label="Selecione o Locutor Principal" 
-                    options={conteudo}
+                </div>
+                <div className="col l3 s6">
+                  <Selected
+                    name="minuto"
+                    label="Selecione os minutos"
+                    options={minutos}
+                    multiple={true}
                   />
-               </div>
-               <div className="col l6 s12">
-                <Selected 
-                    name="programa" 
-                    label="Selecione o nome do Programa" 
-                    options={programa}
-                  />
-               </div>
+                </div>
               </div>
-              <div style={{marginTop: '5%'}} className="col s12">
-                <button style={{zIndex: 0}} className="btn waves-effect waves-light" type="submit" name="action">Cadastrar
+              <div style={{ marginTop: "5%" }} className="col s12">
+                <button
+                  style={{ zIndex: 0 }}
+                  className="btn waves-effect waves-light"
+                  type="submit"
+                  name="action"
+                >
+                  Cadastrar
                   <i className="material-icons right">send</i>
                 </button>
               </div>
             </Form>
           </div>
-          <br/>
+          <br />
           <Table />
         </div>
       </div>
--- src/pages/Dashboard/Programa/Table.js
+++ src/pages/Dashboard/Programa/Table.js
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { toast } from 'react-toastify'
-import api from '../../../services/api';
-import { Link } from 'react-router-dom';
+import { toast } from "react-toastify";
+import api from "../../../services/api";
+import { Link } from "react-router-dom";
 
 // import { Container } from './styles';
 
 export default function Table() {
-  const [programations, setProgramations] = useState([])
+  const [programations, setProgramations] = useState([]);
 
   useEffect(() => {
     loadProgramacaoReferent();
-  }, [])
+  }, []);
 
-  async function loadProgramacaoReferent(){
-    const response = await api.get('programacaos');
+  async function loadProgramacaoReferent() {
+    const response = await api.get("programacaos");
     setProgramations(response.data);
   }
 
-  async function handlDelete(data){
+  async function handlDelete(data) {
     try {
-      await api.delete(`programacaos/${data}`)
-      const response = await api.get('programacaos');
+      await api.delete(`programacaos/${data}`);
+      const response = await api.get("programacaos");
       setProgramations(response.data);
-      toast.success('Programação deletada com sucess')
-    } catch(err){
-      toast.error('Listagem vázia')
+      toast.success("Programação deletada com sucess");
+    } catch (err) {
+      toast.error("Listagem vázia");
     }
   }
 
   return (
     <div className="row">
-            <h5>Programações já cadastradas</h5>
-            <table>
+      <h5>Programações já cadastradas</h5>
+      <table>
         <thead>
           <tr>
-              <th>Programa</th>
-              <th>Horarios</th>
-              <th>Dia</th>
-              <th>Deletar</th>
-              <th>Atualizar</th>
+            <th>Programa</th>
+            <th>Horarios</th>
+            <th>Dia</th>
+            <th>Deletar</th>
+            <th>Atualizar</th>
           </tr>
         </thead>
 
@@ -50,15 +50,21 @@ export default function Table() {
               <td>{el.horario}</td>
               <td>{el.dia ? el.dia.nome : null}</td>
               <td>
-                <button onClick={() => handlDelete(el.id)} className="btn-floating waves-effect waves-light red"><i className="material-icons">delete</i></button>
+                <button
+                  style={{ zIndex: 10 }}
+                  onClick={() => handlDelete(el.id)}
+                  className="btn-floating waves-effect waves-light red"
+                >
+                  <i className="material-icons">delete</i>
+                </button>
               </td>
               <td>
                 <Link to={`attProgramacao/${el.id}`}> Atualizar </Link>
               </td>
-          </tr>
+            </tr>
           ))}
         </tbody>
       </table>
-          </div>
+    </div>
   );
 }
